1ffd32ee970880515e10131decaf4b1da73c5727
BaptisteVolle <baptistevolle09@gmail.com>
2024-04-14 12:01:10 -0400

    added a project

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -7,25 +7,33 @@ import { Project } from './project';
 export class ProjectService {
 
   projectList: Project[] = [
-    {
-      id : 'gameoflife',
-      name : 'Game of Life ',
-      description : 'The Game of Life is a cellular automaton, come fill some cell and see what happens next.',
-      url : 'https://baptistevolle.github.io/gameoflife/',
-    },
+   
 
     {
       id : 'worldmap',
       name : 'Interactive World Map',
-      description : 'An interactive world map where you can display country by color based on differents data, and also display multiple information on country selection',
+      description : 'An interactive world map where you can display country by color based on differents data, and also display multiple information on country selection, Made with Angular V17',
       url: 'https://baptistevolle.github.io/worldmap/',
     },
     {
       id : 'nonogram',
       name : 'A nonogram game',
-      description : 'A little nonogram game allowing the users to generate random grid and try to solve them as fast as possible',
+      description : 'A little nonogram game allowing the users to generate random grid and try to solve them as fast as possible, Made with Angular V17',
       url: 'https://baptistevolle.github.io/nonogram/',
     },
+    {
+      id : 'city-map',
+      name : 'A map of world cities',
+      description : 'A map of world cities with more than 100,000 inhabitants. with cursor to show/hide above a certain value, Made with Angular V17',
+      url: 'https://baptistevolle.github.io/city-map/',
+    },
+    {
+      id : 'gameoflife',
+      name : 'Game of Life ',
+      description : 'The Game of Life is a cellular automaton, come fill some cell and see what happens next, Made with Angular V17',
+      url : 'https://baptistevolle.github.io/gameoflife/',
+    },
+
   ]
   
   getAllProjects(): Project[] {
